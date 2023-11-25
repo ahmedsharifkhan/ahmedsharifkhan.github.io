ad5cd68549086efa57c1aad678b5a78919fa0c41
Ahmed Sharif Khan <abdunnoor540@gmail.com>
2023-11-26 02:06:52 +0600

    sdfgs

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,45 +1,29 @@
----
-layout: null
-sitemap: false
----
-
 document.addEventListener('DOMContentLoaded', function () {
-  var index = lunr(function () {
-    this.field('title', { boost: 10 });
-    this.field('content');
-    this.ref('id');
-  });
-
-  {% assign count = 0 %}
-  {% for post in site.posts %}
-    index.add({
-      title: {{ post.title | jsonify }},
-      content: {{ post.content | jsonify }},
-      id: {{ count }}
-    });
-    {% assign count = count | plus: 1 %}
-  {% endfor %}
-
-  var searchInput = document.getElementById('search-input');
-  var searchResults = document.getElementById('search-results');
+  const searchInput = document.getElementById('searchInput');
+  const searchResults = document.getElementById('searchResults');
 
   searchInput.addEventListener('input', function () {
-    var query = this.value;
-    var results = index.search(query);
-
+    const query = searchInput.value.toLowerCase();
     searchResults.innerHTML = '';
 
-    if (results.length > 0) {
-      results.forEach(function (result) {
-        var item = document.createElement('li');
-        var link = document.createElement('a');
-        link.href = '{{ site.baseurl }}{{ site.posts[result.ref].url }}';
-        link.textContent = '{{ site.posts[result.ref].title }}';
-        item.appendChild(link);
-        searchResults.appendChild(item);
-      });
-    } else {
-      searchResults.innerHTML = 'No results found';
+    if (query.length > 0) {
+      fetch('/search.json')
+        .then(response => response.json())
+        .then(data => {
+          const results = data.filter(item =>
+            item.title.toLowerCase().includes(query) ||
+            item.content.toLowerCase().includes(query)
+          );
+
+          results.forEach(result => {
+            const li = document.createElement('li');
+            const a = document.createElement('a');
+            a.href = result.url;
+            a.textContent = result.title;
+            li.appendChild(a);
+            searchResults.appendChild(li);
+          });
+        });
     }
   });
 });
--- search.js
+++ search.js
@@ -1,45 +1,13 @@
----
-layout: null
-sitemap: false
----
-
-document.addEventListener('DOMContentLoaded', function () {
-  var index = lunr(function () {
-    this.field('title', { boost: 10 });
-    this.field('content');
-    this.ref('id');
-  });
-
-  {% assign count = 0 %}
-  {% for post in site.posts %}
-    index.add({
-      title: {{ post.title | jsonify }},
-      content: {{ post.content | jsonify }},
-      id: {{ count }}
-    });
-    {% assign count = count | plus: 1 %}
-  {% endfor %}
-
-  var searchInput = document.getElementById('search-input');
-  var searchResults = document.getElementById('search-results');
-
-  searchInput.addEventListener('input', function () {
-    var query = this.value;
-    var results = index.search(query);
-
-    searchResults.innerHTML = '';
-
-    if (results.length > 0) {
-      results.forEach(function (result) {
-        var item = document.createElement('li');
-        var link = document.createElement('a');
-        link.href = '{{ site.baseurl }}{{ site.posts[result.ref].url }}';
-        link.textContent = '{{ site.posts[result.ref].title }}';
-        item.appendChild(link);
-        searchResults.appendChild(item);
-      });
-    } else {
-      searchResults.innerHTML = 'No results found';
-    }
-  });
-});
+[
+  {
+    "title": "Page 1",
+    "url": "/page-1",
+    "content": "This is the content of page 1."
+  },
+  {
+    "title": "Page 2",
+    "url": "/page-2",
+    "content": "This is the content of page 2."
+  },
+  // Add more pages as needed
+]
