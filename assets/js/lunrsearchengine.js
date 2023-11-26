239ae8646b4a880c33f8f9d319faef4bfd346f12
Ahmed Sharif Khan <abdunnoor540@gmail.com>
2023-11-26 06:30:28 +0600

    ssssss

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -35,7 +35,7 @@ function lunr_search(term) {
     $('#lunrsearchresults').show(400);
     $("body").addClass("modal-open");
 
-    document.getElementById('lunrsearchresults').innerHTML = '<div id="resultsmodal" class="modal fade show d-block" tabindex="-1" role="dialog" aria-labelledby="resultsmodal"> <div class="modal-dialog shadow-lg" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" class="close" id="btnx" data-dismiss="modal" aria-label="Close"> &times; </button> </div> <div class="modal-body"> <ul class="mb-0"> </ul> </div> <div class="modal-footer"><button id="btnx" type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button></div></div> </div></div>';
+    document.getElementById('lunrsearchresults').innerHTML = '<div id="resultsmodal" class="modal fade show d-block" tabindex="-1" role="dialog" aria-labelledby="resultsmodal"> <div class="modal-dialog modal-lg" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" class="close" id="btnx" aria-label="Close"> &times; </button> </div> <div class="modal-body"> <ul class="mb-0"> </ul> </div> </div> </div></div>';
     if (term) {
         document.getElementById('modtit').innerHTML = "<h5 class='modal-title'>Search results for '" + term + "'</h5>" + document.getElementById('modtit').innerHTML;
         //put results on the screen.
@@ -53,12 +53,12 @@ function lunr_search(term) {
             document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>Sorry, no results found. Close & try a different search!</li>";
         }
     }
-    return false;
-}
 
-$(function () {
-    $("#lunrsearchresults").on('click', '#btnx', function () {
+    // Event binding for close button
+    $('#btnx').on('click', function () {
         $('#lunrsearchresults').hide(5);
         $("body").removeClass("modal-open");
     });
-});
+
+    return false;
+}
